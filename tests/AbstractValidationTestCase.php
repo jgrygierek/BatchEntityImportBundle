dc7b8d769bf77fcd1dcf392bb869d4465d725186
Jarosław Grygierek <jaroslawgrygierek@gmail.com>
2020-05-02 21:31:58 +0200

    tests

---
+++ tests/AbstractValidationTestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JG\BatchEntityImportBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class AbstractValidationTestCase extends TestCase
+{
+    protected ValidatorInterface $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = Validation::createValidatorBuilder()->enableAnnotationMapping()->getValidator();
+    }
+
+    protected function getErrors(object $entity): array
+    {
+        $errors = $this->validator->validate($entity);
+
+        return iterator_to_array($errors);
+    }
+}
