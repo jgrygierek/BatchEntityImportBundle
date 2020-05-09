202852c52ca825e46390a82e7087e24a3c8ca342
Jarosław Grygierek <jaroslawgrygierek@gmail.com>
2020-05-09 19:12:23 +0200

    tests

--- tests/Controller/ImportControllerTraitTest.php
+++ tests/Controller/ImportControllerTraitTest.php
@@ -43,15 +43,24 @@ class ImportControllerTraitTest extends WebTestCase
         $this->assertTrue($this->client->getResponse()->isRedirect('/jg_batch_entity_import_bundle/import'));
         $this->client->followRedirect();
         $this->assertTrue($this->client->getResponse()->isSuccessful());
-
+        $this->assertStringContainsString('Data has been imported', $this->client->getResponse()->getContent());
         $this->assertCount(2, $repository->findAll());
 
         /** @var TranslatableEntity|null $item */
         $item = $repository->find(2);
 
         $this->assertNotEmpty($item);
-        $this->assertSame('test2', $item->getTestProperty());
-//        $this->assertSame('test2_en', $item->translate('en')->getTestTranslationProperty());
-//        $this->assertSame('test2_pl', $item->translate('pl')->getTestTranslationProperty());
+    }
+
+    public function testImportFileWrongExtension(): void
+    {
+        $this->client->request('GET', '/jg_batch_entity_import_bundle/import');
+
+        $uploadedFile = __DIR__ . '/../Fixtures/Resources/test.txt';
+        $this->client->submitForm('btn-submit', ['file_import[file]' => $uploadedFile]);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertStringContainsString('Wrong file extension.', $this->client->getResponse()->getContent());
+        $this->assertStringContainsString('id="file_import_file"', $this->client->getResponse()->getContent());
     }
 }
