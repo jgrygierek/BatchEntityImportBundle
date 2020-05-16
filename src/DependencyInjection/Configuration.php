6296afac26cd37aec3fca73bcf90d26e7043b8dc
Jarosław Grygierek <jaroslawgrygierek@gmail.com>
2020-05-16 20:10:23 +0200

    templates

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace JG\BatchEntityImportBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('batch_entity_import');
+
+        $nodeBuilder = $treeBuilder->getRootNode()->children();
+        $this->addTemplatesConfig($nodeBuilder);
+        $nodeBuilder->end();
+
+        return $treeBuilder;
+    }
+
+    private function addTemplatesConfig(NodeBuilder $parentBuilder): void
+    {
+        $builder = $parentBuilder->arrayNode('templates')->addDefaultsIfNotSet()->children();
+
+        $this->addNodeConfig($builder, 'select_file', '@BatchEntityImport/select_file.html.twig');
+        $this->addNodeConfig($builder, 'edit_matrix', '@BatchEntityImport/edit_matrix.html.twig');
+        $this->addNodeConfig($builder, 'layout', '@BatchEntityImport/layout.html.twig');
+
+        $parentBuilder->end()->end();
+    }
+
+    private function addNodeConfig(NodeBuilder $builder, string $name, string $value): void
+    {
+        $builder
+            ->scalarNode($name)
+            ->defaultValue($value)
+            ->cannotBeEmpty()
+            ->end();
+    }
+}
---
+++ src/Twig/TemplateExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JG\BatchEntityImportBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use UnexpectedValueException;
+
+class TemplateExtension extends AbstractExtension
+{
+    private array  $availableTemplates;
+
+    public function __construct(array $availableTemplates = [])
+    {
+        $this->availableTemplates = $availableTemplates;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('batch_entity_import_template', [$this, 'getTemplate']),
+        ];
+    }
+
+    public function getTemplate(string $name): string
+    {
+        if (array_key_exists($name, $this->availableTemplates)) {
+            return $this->availableTemplates[$name];
+        }
+
+        throw new UnexpectedValueException('Template not found.');
+    }
+}
